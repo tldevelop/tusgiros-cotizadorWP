14d744089271c6582f65ca73529a1019f91be8ff
tldevelop <splintercell.286@gmail.com>
2023-02-07 22:29:50 -0600

    adding initial JS

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
 window.onload = function () {
-    
+    //FORM FIELDS
+    const sendAmount = document.querySelector('#send-amount');
+    const originRegion = document.querySelector('#origin-region');
+    const receiveAmount = document.querySelector('#receive-amount');
+    const destinyRegion = document.querySelector('#dest-region');
+    // END FORM FIELDS
+
+    //TODAY VALUE SELECTORS
+    const originCurrency = document.querySelector('#origen');
+    const conversion = document.querySelector('#conversion');
+    const destinyCurrency = document.querySelector('#destino');
+    //END TODAY VALUE SELECTORS
+
+    //SET INITIAL TODAY VALUES
+    originCurrency.innerHTML = originRegion.value;
+    destinyCurrency.innerHTML = destinyRegion.value;
+
+    //ON TODAY VALUES CHANGE
+    originRegion.onchange = (e) => {
+        originCurrency.innerHTML = e.target.value;
+    }
+    destinyRegion.onchange = (e) => {
+        destinyCurrency.innerHTML = e.target.value;
+    }
 }
